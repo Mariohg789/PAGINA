80de0238b72b300fbdf051b435bf3a927c650486
Manulick <nava.arevalo@hotmail.com>
2019-07-18 16:20:49 -0500

    borre metodo consultaTodo()

--- LoginOmar/FuncionesBD.php
+++ LoginOmar/FuncionesBD.php
@@ -26,51 +26,6 @@
 		}
         return array($correo,$pass);
 	}
-/*
-	function consultaTodo()
-	{
-		$conex = conectarBD();
-		$select = "select * from Cliente";
-		$reseaultSet = sqlsrv_query($conex,$select);
-		try {
 
-
-		echo "<table>
-						<tr>
-							<th>Id</th>
-							<th>Nombre</th>
-							<th>Apellido</th>
-							<th>Contrasena</th>
-							<th>Correo</th>
-							<th>Telefono</th>
-							<th>Estado</th>
-							<th>Eliminar</th>
-						</tr>
-						
-				";
-
-				while ($arrayFilas = sqlsrv_fetch_array($reseaultSet)) {
-					echo "<tr>
-							<td> ".$arrayFilas['Id']."</td>
-							<td> ".$arrayFilas['Nombre']."</td>
-							<td> ".$arrayFilas['ApellidoP']."</td>
-							<td> ".$arrayFilas['Contrasena']."</td>
-							<td> ".$arrayFilas['Correo']."</td>
-							<td> ".$arrayFilas['Telefono']."</td>
-							<td> ".$arrayFilas['Estado']."</td>
-							<td><a href='../Eliminar/Eliminar.php?id=".$arrayFilas['Id']."'>Eliminar</a></td>
-					</tr>";
-				}
-				echo "</table>";				
-				
-				
-			}
-		catch (Exception $e) {
-			echo '<script> alert("Fatal Error");</script>' ;
-			echo "<META HTTP-EQUIV = 'REFRESH' CONTENT='2 ; URL= index.html'>";
-		}
-	}
-*/
 ?>
 
-45621
