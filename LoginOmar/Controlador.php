efaf6c4751814f230412bf37eb465b81a854a9fa
Manulick <nava.arevalo@hotmail.com>
2019-07-18 20:07:24 -0500

    cambio de redireccionamiento a javaScript

--- LoginOmar/Controlador.php
+++ LoginOmar/Controlador.php
@@ -4,7 +4,7 @@
 	<head>...</head>
 
             <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
-            <script src="../JS/mensajes.js"></script>
+            <script src="../mensajes_alertas_js/mensajes.js"></script>
 	<BODY>
 		<?php
 			require 'FuncionesBD.php';
@@ -17,7 +17,7 @@
 				if (array($correo,$password) == validarUsuario($correo)) 
 				{
 				
-					echo '<script> bienvenido();</script>' ;
+					echo '<script> alert(bienvenido());</script>' ;
 					
 				}
 				else
