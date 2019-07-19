4d42539e18a82cab5dfb579676358a052ec66bce
Mario Herrera <mariohg120@hotmail.com>
2019-07-18 19:24:22 -0500

    Se agregaron los mensajes

--- LoginOmar/Controlador.php
+++ LoginOmar/Controlador.php
@@ -4,7 +4,7 @@
 	<head>...</head>
 
             <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
-            <script src="../JS/mensajes.js"></script>
+            <script src="../mensajes_alertas_js/mensajes.js"></script>
 	<BODY>
 		<?php
 			require 'FuncionesBD.php';
@@ -17,7 +17,7 @@
 				if (array($correo,$password) == validarUsuario($correo)) 
 				{
 				
-					echo '<script> alert("Bienvenido");</script>' ;
+					echo '<script> bienvenido();</script>' ;
 					echo "<META HTTP-EQUIV = 'REFRESH' CONTENT='0 ; URL= ../Menu Principal/Index.html'>";
 					session_start();
 					$_SESSION["SessionActiva"] = buscarId($_POST['txtUser']);
@@ -25,7 +25,7 @@
 				}
 				else
 				{
-					echo '<script> alert("Error al regresar");</script>' ;
+					echo '<script> erroLogin();</script>' ;
 					echo "<META HTTP-EQUIV = 'REFRESH' CONTENT='0 ; URL= ../Menu Principal/Index.html'>" ;	
 				}
 				
