8d0c89caa1ac52f530dce77159fd7d0a2d3a9138
Mario Herrera <mariohg120@hotmail.com>
2019-07-18 19:11:56 -0500

    Agregar carpeta de Alertas y Mensajes

---
+++ mensajes_alertas_js/mensajes.js
@@ -0,0 +1,83 @@
+//Resgistro cliente
+function resgitroCliente()
+{
+  Swal.fire({
+    position: 'top',
+    type: 'success',
+    title: 'Usuario registrado',
+    showConfirmButton: false,
+    timer: 2000
+  }).then(function(){window.location="#"})
+}
+//Error de cliente
+function errorCliente()
+{
+  Swal.fire({
+    position: 'top',
+    type: 'error',
+    title: 'Usuario no registrado',
+    showConfirmButton: false,
+    timer: 2000
+  }).then(function(){window.location="#"})
+}
+//Bienvenido 
+function bienvenido()
+{
+  Swal.fire({
+    position: 'top',
+    type: 'success',
+    title: 'Bienvenido',
+    showConfirmButton: false,
+    timer: 2000
+  }).then(function(){window.location="#"})
+
+} 
+//Error al entrada 
+function erroLogin()
+{
+    Swal.fire('MARVAN MECHANIC´S', 'Tu usuario y/o contraseña no validos', 'error').then(function(){window.location="#"});
+
+}
+//Modificar Datos 
+function modificarDato()
+{
+  Swal.fire({
+    position: 'top-end',
+    type: 'CORRECTO',
+    title: 'Se ha modificado',
+    showConfirmButton: false,
+    timer: 1500
+  }).then(function(){window.location="#"})
+}
+//Eliminar Datos 
+function eliminarDato()
+{
+  Swal.fire({
+    position: 'top-end',
+    type: 'CORRECTO',
+    title: 'Se ha eliminado',
+    showConfirmButton: false,
+    timer: 1500
+  }).then(function(){window.location="#"})
+}
+//Reserva
+function reserva()
+{
+  Swal.fire(
+    'Reservacion',
+    'Tu reservacion se hizo con exito',
+    'success'
+  )
+}
+//Error de reservación 
+function errorReserva()
+{
+  Swal.fire(
+    'Good job!',
+    'You clicked the button!',
+    'error'
+  )
+}
+//
+
+
